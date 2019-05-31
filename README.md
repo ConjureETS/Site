412e83bfa968e481d11f0f19f5c97f96ff0327e7
Maxime Di Lullo <massimo.dil@gmail.com>
2019-05-31 12:29:24 -0400

    Project SetUp
    Add node_modules, gulp (to watch and compile js and sass) + create project directories

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Site
+# Conjure Site
 Site web officiel du club Conjure de l'ÉTS
+
+## Installation
+1) Clone projet de git
+2) Faire `npm install` en ligne de commande
+
+## Dépendances
+- CSS Framework : [Bulma v.0.7.5](https://bulma.io/)
+- Compilation par Gulp
+
+## Compilation
+Lorsque vous faites des changements dans le JavaScript ou le SASS, lancez la commande de commande `gulp watch`.
+Celui-ci regardera les changements fait au fur et à mesure et compilera les fichiers pour vous.
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let gulp = require('gulp');
+let uglify = require('gulp-uglify');
+let rename = require('gulp-rename');
+let concat = require('gulp-concat');
+let Fiber = require('fibers');
+let cleanCSS = require('gulp-clean-css');
+let sass = require('gulp-sass');
+sass.compiler = require('sass');
+
+const directories = {
+    source: './resources/',
+    destination: './public/'
+};
+
+gulp.task('sass', function () {
+    return gulp.src(directories.source + 'sass/**/*.scss')
+        .pipe(sass({fiber: Fiber}).on('error', sass.logError))
+        .pipe(concat('main.css'))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(directories.destination + 'css/'));
+});
+
+gulp.task('js', function () {
+    return gulp.src(directories.source + 'js/**/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(directories.destination + 'js/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch([directories.source + 'sass/**/*.scss'], gulp.series('sass'));
+    gulp.watch([directories.source + 'js/**/*.js'], gulp.series('js'));
+
+});
