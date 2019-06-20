a9a0760f713ce583886c5b5b805e36b8b4e4bea5
Maxime Di Lullo <massimo.dil@gmail.com>
2019-06-20 11:31:56 -0400

    Start Section Activités

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,17 @@
 (function() {
     addCopyrightCurrentDate();
+
+    document.querySelectorAll('section#activites button').forEach(function(item) {
+        item.addEventListener('click', showActivityContent);
+    })
 })();
 
 function addCopyrightCurrentDate() {
     document.querySelector('footer').querySelector('p').innerHTML += ' ' + new Date().getFullYear();
+}
+
+function showActivityContent(e) {
+    e.target.parentElement.parentElement.querySelector('.headline').classList.toggle('toggle-visibility');
+    e.target.parentElement.parentElement.querySelector('.content').classList.toggle('slide');
+    e.target.parentElement.parentElement.querySelector('.bg-img').classList.toggle('slide');
 }
