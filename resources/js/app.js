51ec9136d342aa9fbf0433e5fd1983ba7b180ce7
Massimo Di Lullo <massimo.dil@gmail.com>
2019-08-14 13:53:34 -0400

    Correction Sponsors Image Path + Add Competitions Section

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,19 @@
     document.querySelectorAll( '.year-animated' ).forEach(function(item) {
         animateNumbers( item );
     });
+
+    bulmaCarousel.attach('.slider', {
+        autoplay: true,
+        pagination: false,
+        loop: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        icons: {
+            previous: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 100 100" width="100" height="100"><defs><clipPath id="_clipPath_Rmt62eEg1OXF13UQV6A0tZ7nRrSHuqzl"><rect width="100" height="100"/></clipPath></defs><g clip-path="url(#_clipPath_Rmt62eEg1OXF13UQV6A0tZ7nRrSHuqzl)"><path d=" M 68.738 87.477 L 68.738 87.477 L 31.262 50 L 68.738 12.523" fill="none" vector-effect="non-scaling-stroke" stroke-width="8" stroke="rgb(132,132,132)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="3"/></g></svg>',
+            next: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 100 100" width="100" height="100"><defs><clipPath id="_clipPath_sc80dOHRo7GcAKylIDWHu4HjiUHjgH8R"><rect width="100" height="100"/></clipPath></defs><g clip-path="url(#_clipPath_sc80dOHRo7GcAKylIDWHu4HjiUHjgH8R)"><path d=" M 39 12.523 L 39 12.523 L 76.477 50 L 39 87.477" fill="none" vector-effect="non-scaling-stroke" stroke-width="8" stroke="rgb(132,132,132)" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="3"/></g></svg>'
+        }
+    });
 })();
 
 function addCopyrightCurrentDate() {
